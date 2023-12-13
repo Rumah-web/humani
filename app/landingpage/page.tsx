c5c2b78d8b333bfe69934350875fbc66d8244a7a
Iwan Susanto <iwan@goperasi.id>
2023-12-13 11:11:55 +0700

    layput

---
+++ app/landingpage/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+      <div className="w-1/2 bg-white p-4">ABOUUUUT</div>
+    </main>
+  );
+}
