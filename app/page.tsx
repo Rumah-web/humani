5eadfdc9ddddef1b93212eeab605ea961309c9db
Iwan Susanto <iwansusanto@iwans-air.0.43.168.192.in-addr.arpa>
2024-01-11 21:33:47 +0700

    branding - icon komitmen

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,8 @@ export default function Home() {
     `/testimoni/testimoni-3.jpeg`,
   ];
 
+  const ourCustomer = [1,2,3,4,5,6,7,8]
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <section id="wellcome" className='w-full h-screen flex flex-col bg-no-repeat bg-right-bottom 
@@ -115,34 +117,13 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-              <h2 className='text-white md:text-5xl text-2xl text-center mt-12'>Komitmen Kami</h2>
+              <div className='relative'>
+                <h2 className='text-white md:text-4xl text-2xl text-center mt-12 font-bold'>Komitmen Kami</h2>
+              </div>
               <div className='z-10'>
                 <div
-                      className="border border-white flex items-center justify-between rounded-full px-8 py-2.5 w-full bg-[#88171d] my-4"
+                      className="border border-white flex items-center justify-between rounded-full px-8 py-2.5 w-full bg-[#e7e8ea] my-4"
                     >
-                      <div className="flex flex-col w-full space-y-1 text-white items-center justify-center">
-                        <div
-                          className="flex bg-white rounded-full h-9 w-9 items-center justify-center text-[#88171d]"
-                        >
-                          <svg
-                            width="20"
-                            height="20"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              fill="currentColor"
-                              d="M7 5v10c0 1.11-.89 2-2 2H4c-.55 0-1-.45-1-1v-3H1v3c0 1.66 1.34 3 3 3h1c2.21 0 4-1.79 4-4V5m14 8.38l-1.23-1.23A3.905 3.905 0 0 0 19 11h-2v2h2c.5 0 1 .2 1.35.56l.35.35l-1.2.69c-.46.26-.97.4-1.5.4h-1c-.56 0-1-.44-1-1V6h-2v3.81l-.62-.69C12.7 8.37 11.6 8 10.59 8H10v2h.59c.64 0 1.25.25 1.7.71l1.71 1.7V14c0 1.12-.88 2-2 2h-2v2h2c1.37 0 2.5-.7 3.17-1.76A2.5 2.5 0 0 0 17 17h1c.88 0 1.74-.23 2.5-.67l2.5-1.44Z"
-                            />
-                          </svg>
-                        </div>
-                        <h3
-                          style={{ fontSize: `0.5rem` }}
-                          className="flex text-center md:h-6 h-6 items-center"
-                        >
-                          Halal
-                        </h3>
-                      </div>
                       <div className="flex flex-col w-full space-y-1 text-white items-center justify-center">
                         <div
                           className="flex bg-white rounded-full h-9 w-9 items-center justify-center text-[#88171d]"
@@ -212,8 +193,65 @@ export default function Home() {
                           Baik Bersih Cantik
                         </h3>
                       </div>
+                      <div className="flex flex-col w-full space-y-1 text-white items-center justify-center">
+                        <div
+                          className="flex bg-[#88171d] rounded-full h-9 w-9 items-center justify-center text-white"
+                        >
+                          <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M7 5v10c0 1.11-.89 2-2 2H4c-.55 0-1-.45-1-1v-3H1v3c0 1.66 1.34 3 3 3h1c2.21 0 4-1.79 4-4V5m14 8.38l-1.23-1.23A3.905 3.905 0 0 0 19 11h-2v2h2c.5 0 1 .2 1.35.56l.35.35l-1.2.69c-.46.26-.97.4-1.5.4h-1c-.56 0-1-.44-1-1V6h-2v3.81l-.62-.69C12.7 8.37 11.6 8 10.59 8H10v2h.59c.64 0 1.25.25 1.7.71l1.71 1.7V14c0 1.12-.88 2-2 2h-2v2h2c1.37 0 2.5-.7 3.17-1.76A2.5 2.5 0 0 0 17 17h1c.88 0 1.74-.23 2.5-.67l2.5-1.44Z"
+                            />
+                          </svg>
+                        </div>
+                        <h3
+                          style={{ fontSize: `0.8rem` }}
+                          className="flex text-center md:h-6 h-6 items-center font-bold text-[#88171d]"
+                        >
+                          Segar, Baik dan Halal
+                        </h3>
+                      </div>
                     </div>
                   </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="customer" className='w-full bg-white'>
+        <h2 className='text-[#88171d] md:text-4xl text-2xl text-center mt-12 font-bold'>Pelanggan Setia Kami</h2>
+        <div className="flex flex-col justify-center pt-6 space-y-2 items-center">
+              {ourCustomer.map((customer, i) => {
+                return <div key={i} className="w-96 md:px-0 px-4">
+                <Image
+                  src={`/client/customer-${customer}.png`}
+                  alt="Humani Food Customer"
+                  width={100}
+                  height={24}
+                  priority
+                  style={{ width: `100%` }}
+                /></div>
+              })}
+            </div>
+      </section>
+      <section id="gallery" className='w-full bg-white'>
+        <h2 className='text-[#88171d] md:text-4xl text-2xl text-center mt-12 font-bold'>Gallery Humani Catering Service</h2>
+        <div className='pt-8 pb-2'>
+          <Carosel images={testimoni} />
+        </div>
+      </section>
+      <section id="chat-wa" className='py-8 bg-white w-full flex justify-center'>
+        <div className='w-fit'>
+          <div className="border border-[#88171d] py-1.5 px-1.5 rounded-full">
+            <div className="flex pl-6 pr-4 py-2.5 rounded-full items-center text-white bg-gradient-to-r from-[#88171d] to-[#d83831]">
+              <svg width="40" height="40" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                <path fill="currentColor" d="M23.328 19.177c-.401-.203-2.354-1.156-2.719-1.292c-.365-.13-.63-.198-.896.203c-.26.391-1.026 1.286-1.26 1.547s-.464.281-.859.104c-.401-.203-1.682-.62-3.203-1.984c-1.188-1.057-1.979-2.359-2.214-2.76c-.234-.396-.026-.62.172-.818c.182-.182.401-.458.604-.698c.193-.24.255-.401.396-.661c.13-.281.063-.5-.036-.698s-.896-2.161-1.229-2.943c-.318-.776-.651-.677-.896-.677c-.229-.021-.495-.021-.76-.021s-.698.099-1.063.479c-.365.401-1.396 1.359-1.396 3.297c0 1.943 1.427 3.823 1.625 4.104c.203.26 2.807 4.26 6.802 5.979c.953.401 1.693.641 2.271.839c.953.302 1.823.26 2.51.161c.76-.125 2.354-.964 2.688-1.901c.339-.943.339-1.724.24-1.901c-.099-.182-.359-.281-.76-.458zM16.083 29h-.021c-2.365 0-4.703-.641-6.745-1.839l-.479-.286l-5 1.302l1.344-4.865l-.323-.5a13.166 13.166 0 0 1-2.021-7.01c0-7.26 5.943-13.182 13.255-13.182c3.542 0 6.865 1.38 9.365 3.88a13.058 13.058 0 0 1 3.88 9.323C29.328 23.078 23.39 29 16.088 29zM27.359 4.599C24.317 1.661 20.317 0 16.062 0C7.286 0 .14 7.115.135 15.859c0 2.792.729 5.516 2.125 7.927L0 32l8.448-2.203a16.13 16.13 0 0 0 7.615 1.932h.005c8.781 0 15.927-7.115 15.932-15.865c0-4.234-1.651-8.219-4.661-11.214z"/>
+              </svg>
+              <div className="text-xl pl-2">Chat Admin Disini Yaa!</div>
             </div>
           </div>
         </div>
