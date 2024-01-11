047620267fe01cbfca875551648501a884b598b7
Iwan Susanto <iwansusanto@iwans-air.0.43.168.192.in-addr.arpa>
2024-01-11 23:09:07 +0700

    branding - ragam menu

--- app/page.tsx
+++ app/page.tsx
@@ -277,25 +277,94 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section id="menu" className='w-full bg-white flex justify-center'>
+      <section id="menu" className='w-full relative bg-white flex justify-center pb-4'>
+        <div className='absolute top-0 left-0 bg-contain bg-no-repeat w-full h-52 bg-left-top' style={{
+              backgroundImage: `url(/bg/bg-ragam-menu-left-top.png)`,
+            }}></div>
+        <div className='absolute top-0 right-0 bg-contain bg-no-repeat w-full h-52 bg-right-top' style={{
+              backgroundImage: `url(/bg/bg-ragam-menu-right-top.png)`,
+            }}></div>
         <div className='flex max-w-5xl w-full flex-col'>
           <h2 className='text-[#88171d] md:text-4xl text-2xl text-center mt-12 font-bold'>Ragam Menu</h2>
           <div className='pt-16 pb-8 grid grid-rows-2 grid-flow-col gap-12 text-[#88171d]'>
             {menus.map((menu, i) => {
-              return <div key={i} className='flex flex-col pb-8 items-center'>
-                <h3 className='font-bold text-xl text-center'>{menu.title}</h3>
-                <div className='w-full h-72 bg-[#88171d] my-2'></div>
+              return <div key={i} className='flex flex-col items-center'>
+                <h3 className='font-bold text-xl text-center pb-2'>{menu.title}</h3>
+                <div className='w-full bg-[#88171d] my-2 h-72 bg-cover bg-no-repeat bg-center' style={{
+                      backgroundImage: `url(/menu/menu-tumpeng-mini.png)`}}>
+                </div>
                 <div className='pt-2 text-center'>{menu.description}</div>
               </div>
             })}
           </div>
-          <h3 className='text-center text-[#88171d] text-xl'>
+          <h3 className='text-center text-[#88171d] text-xl my-8'>
             Acara mepet waktu kepepet, butuh katering yang satset? <br />
             Segera hub Admin SatSet untuk mendapatkan <br />
             solusi masalah konsumsi. Satu Setengah Jam Siap Antar <br /> 
             kemanapun dan kapanpun dengan minimum 10 porsi.
           </h3>
         </div>
+        <div className='absolute bottom-0 left-0 bg-contain bg-no-repeat w-full h-1/3 bg-left-bottom' style={{
+              backgroundImage: `url(/bg/bg-ragam-menu-left-bottom.png)`,
+            }}></div>
+        <div className='absolute bottom-0 right-0 bg-contain bg-no-repeat w-full h-1/3 bg-right-bottom' style={{
+              backgroundImage: `url(/bg/bg-ragam-menu-right-bottom.png)`,
+            }}></div>
+      </section>
+      <section id="chat-admin" className='relative flex w-full bg-white justify-center overflow-hidden'>
+        <div className='absolute -bottom-36 -left-36 bg-contain bg-no-repeat w-full h-72 bg-left-bottom' style={{
+                backgroundImage: `url(/logo-red.png)`,
+              }}></div>
+        <div className='absolute top-0 -right-72 bg-contain bg-no-repeat w-full h-[36rem] bg-right-top' style={{
+                backgroundImage: `url(/logo-red.png)`,
+              }}></div>
+        <div className="bg-white opacity-90 absolute w-full h-full"></div>
+        <div className="relative flex flex-col space-y-4 item-center text-center py-4 text-[#88171d]">
+          <div className="flex justify-between md:px-24 px-16 py-8">
+            <div className="animate-bounce">
+                  <Image
+                    src="/icon/arrow-cs.png"
+                    alt="Humani Food CS"
+                    className="-rotate-45"
+                    width={60}
+                    height={20}
+                    priority
+                  />
+                </div>
+                <div className="animate-bounce">
+                  <Image
+                    src="/icon/arrow-cs.png"
+                    alt="Humani Food CS"
+                    width={60}
+                    height={20}
+                    priority
+                  />
+                </div>
+                <div className="animate-bounce">
+                  <Image
+                    src="/icon/arrow-cs.png"
+                    alt="Humani Food CS"
+                    className="rotate-45"
+                    width={60}
+                    height={20}
+                    priority
+                  />
+                </div>
+            </div>
+            <div className="flex justify-center pb-4">
+              <div className="border border-[#88171d] py-1.5 px-1.5 rounded-full">
+                  <div className="flex pl-6 pr-4 py-2.5 rounded-full items-center text-white bg-gradient-to-r from-[#88171d] to-[#d83831]">
+                    <svg width="40" height="40" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                          <path fill="currentColor" d="M23.328 19.177c-.401-.203-2.354-1.156-2.719-1.292c-.365-.13-.63-.198-.896.203c-.26.391-1.026 1.286-1.26 1.547s-.464.281-.859.104c-.401-.203-1.682-.62-3.203-1.984c-1.188-1.057-1.979-2.359-2.214-2.76c-.234-.396-.026-.62.172-.818c.182-.182.401-.458.604-.698c.193-.24.255-.401.396-.661c.13-.281.063-.5-.036-.698s-.896-2.161-1.229-2.943c-.318-.776-.651-.677-.896-.677c-.229-.021-.495-.021-.76-.021s-.698.099-1.063.479c-.365.401-1.396 1.359-1.396 3.297c0 1.943 1.427 3.823 1.625 4.104c.203.26 2.807 4.26 6.802 5.979c.953.401 1.693.641 2.271.839c.953.302 1.823.26 2.51.161c.76-.125 2.354-.964 2.688-1.901c.339-.943.339-1.724.24-1.901c-.099-.182-.359-.281-.76-.458zM16.083 29h-.021c-2.365 0-4.703-.641-6.745-1.839l-.479-.286l-5 1.302l1.344-4.865l-.323-.5a13.166 13.166 0 0 1-2.021-7.01c0-7.26 5.943-13.182 13.255-13.182c3.542 0 6.865 1.38 9.365 3.88a13.058 13.058 0 0 1 3.88 9.323C29.328 23.078 23.39 29 16.088 29zM27.359 4.599C24.317 1.661 20.317 0 16.062 0C7.286 0 .14 7.115.135 15.859c0 2.792.729 5.516 2.125 7.927L0 32l8.448-2.203a16.13 16.13 0 0 0 7.615 1.932h.005c8.781 0 15.927-7.115 15.932-15.865c0-4.234-1.651-8.219-4.661-11.214z"/>
+                    </svg>
+                    <div className="text-xl pl-2">Chat Admin Disini Yaa!</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="review-slider" className='w-full py-24 flex justify-center'>
+        REVIEW SLIDER
       </section>
       <section id="customer" className='w-full bg-white'>
         <h2 className='text-[#88171d] md:text-4xl text-2xl text-center mt-12 font-bold'>Pelanggan Setia Kami</h2>
