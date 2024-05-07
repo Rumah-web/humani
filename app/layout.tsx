e6decc2c37e58acfcefb0b93e7be58301e306a5f
Iwan Susanto <iwansusanto@iwans-air.0.43.168.192.in-addr.arpa>
2024-05-07 12:06:29 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,21 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
+			<head>
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<!-- Google tag (gtag.js) -->
+						<script async src="https://www.googletagmanager.com/gtag/js?id=G-GRZ86KF2WC"></script>
+						<script>
+						  window.dataLayer = window.dataLayer || [];
+						  function gtag(){dataLayer.push(arguments);}
+						  gtag('js', new Date());
+						
+						  gtag('config', 'G-GRZ86KF2WC');
+						</script>`,
+					}}
+				/>
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
