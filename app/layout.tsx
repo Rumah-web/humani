7267789698b0a146db9881ac739d84c158a9f771
Iwan Susanto <iwansusanto@iwans-air.0.43.168.192.in-addr.arpa>
2024-05-13 17:03:33 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<head>
+			{/* <head>
 				<noscript
 					dangerouslySetInnerHTML={{
 						__html: `<!-- Google tag (gtag.js) -->
@@ -33,8 +34,9 @@ export default function RootLayout({
 						</script>`,
 					}}
 				/>
-			</head>
+			</head> */}
 			<body className={inter.className}>{children}</body>
+			<GoogleAnalytics gaId='G-GRZ86KF2WC' />
 		</html>
 	);
 }
